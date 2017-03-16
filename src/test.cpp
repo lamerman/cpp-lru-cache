40992b1e87615af7142fb9b37c6d4ad41b698177
anithakathir <anithaprabakathir@gmail.com>
2017-03-16 18:25:45 +0530

    Capture and return the lru victim to the caller

--- src/test.cpp
+++ src/test.cpp
@@ -7,7 +7,8 @@ const int TEST2_CACHE_CAPACITY = 50;
 
 TEST(CacheTest, SimplePut) {
     cache::lru_cache<int, int> cache_lru(1);
-    cache_lru.put(7, 777);
+    std::pair<int, int> lru_item;
+    cache_lru.put(7, 777, lru_item);
     EXPECT_TRUE(cache_lru.exists(7));
     EXPECT_EQ(777, cache_lru.get(7));
     EXPECT_EQ(1, cache_lru.size());
@@ -20,9 +21,9 @@ TEST(CacheTest, MissingValue) {
 
 TEST(CacheTest1, KeepsAllValuesWithinCapacity) {
     cache::lru_cache<int, int> cache_lru(TEST2_CACHE_CAPACITY);
-
+    std::pair<int, int> lru_item;
     for (int i = 0; i < NUM_OF_TEST2_RECORDS; ++i) {
-        cache_lru.put(i, i);
+        cache_lru.put(i, i, lru_item);
     }
 
     for (int i = 0; i < NUM_OF_TEST2_RECORDS - TEST2_CACHE_CAPACITY; ++i) {
@@ -36,6 +37,28 @@ TEST(CacheTest1, KeepsAllValuesWithinCapacity) {
 
     size_t size = cache_lru.size();
     EXPECT_EQ(TEST2_CACHE_CAPACITY, size);
+}
+
+TEST(CacheTest2, CapturesTheLRUItem) {
+    cache::lru_cache<int, int> cache_lru(3);
+    std::pair<int, int> lru_item;
+
+    EXPECT_FALSE(cache_lru.put(1, 111, lru_item));
+    EXPECT_FALSE(cache_lru.put(2, 222, lru_item));
+    EXPECT_FALSE(cache_lru.put(3, 333, lru_item));
+    EXPECT_TRUE(cache_lru.put(4, 444, lru_item));
+
+    EXPECT_EQ(1, lru_item.first);
+    EXPECT_EQ(111, lru_item.second);
+
+    EXPECT_TRUE(cache_lru.exists(2));
+    EXPECT_TRUE(cache_lru.exists(3));
+    EXPECT_TRUE(cache_lru.exists(4));
+
+    EXPECT_EQ(222, cache_lru.get(2));
+    EXPECT_EQ(333, cache_lru.get(3));
+    EXPECT_EQ(444, cache_lru.get(4));
+
 }
 
 int main(int argc, char **argv) {
